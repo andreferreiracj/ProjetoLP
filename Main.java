efce7026e3fac11fb7e401974d25f3b7d6a3e2f8
andreferreiracj <65228397+andreferreiracj@users.noreply.github.com>
2020-05-23 12:55:09 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+	private static Scanner teclado = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		ArvoreDoJogo inicio;
+		System.out.println("Olá amigo, vamos jogar um jogo interessante.");
+		System.out.println("Pensa em um animal ao calhas e garanto te que advinho em qual estás a pensar");
+
+	}
+
+	
+	 public static ArvoreDoJogo inicio(){  //incompleto
+		 //criação de nodes na arvore binaria
+	     ArvoreDoJogo inicio;
+		 ArvoreDoJogo node1;
+		 ArvoreDoJogo node2;
+		 ArvoreDoJogo node3;
+		 ArvoreDoJogo node4;
+		 ArvoreDoJogo node5;
+		 ArvoreDoJogo node6;
+		 ArvoreDoJogo node7;
+		 
+		 //Perguntas a fazer
+	     final String pergunta1 = "Corre bastante?"; //Ou é a zebra ou o cavalo
+	     final String pergunta2 = "Tem riscas?"; //É a zebra
+	     final String pergunta3 = "É um animal presente no filme Spirit?"; // É o cavalo.
+	     final String pergunta5 = "É pequena?"; //Ou é a galinha, a rato.
+	     final String pergunta6 = "O seu habitat é a agua?"; //Golfinho ou baleia ou polvo.
+	     final String pergunta7 = "Reza a lenda que o bicharoco gosta de queijo?"; //É o rato
+	     final String pergunta8 = "Tem tentaculos?"; // É o Polvo
+	     final String pergunta9 = " O animal necessita de vir á superficie às vezes?"; // é o golfinho
+	     final String pergunta10 = " O animal já ingoliu o pinócio?"; // É a baleia.
+	     final String pergunta11 = "Quatro patas certo?"; //cao ou arminho
+	     final String pergunta12 = "O animal está presente no filme 101 dalmatas?"; //cao
+	     final String pergunta13 = "Hum...É um animal peculiar, branquinho e fofinho?"; //Arminho.
+	     final String pergunta14 = "O animal põe ovos?"; //Galinha
+	      
+	     //Animais a adivinhar
+	      final String ANIMAL1 = "zebra"; 
+	      final String ANIMAL2 = "galinha";
+	      final String ANIMAL3 = "pato";
+	      final String ANIMAL4 = "cao";
+	      final String ANIMAL5 = "arminho";
+	      final String ANIMAL6 = "rato"; 
+	      final String ANIMAL7 = "polvo"; 
+	      final String ANIMAL8 = "cavalo"; 
+	      final String ANIMAL9 = "golfinho";
+	      final String ANIMAL10 = "baleia";
+	      
+	      
+	      
+		return; 
+	    
+
+	      // Create the root node with the question “Are you a mammal?”
+	      
+
+	      
+	   }
+}
