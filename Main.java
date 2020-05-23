1e6e22b830a3d1150b842828eea3e72e06b76e85
andreferreiracj <65228397+andreferreiracj@users.noreply.github.com>
2020-05-23 16:31:32 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -73,23 +73,25 @@ public class Main {
 	      	
 	      	
 	      	//Começar para a direita do inicio.
-	      		node4 = new ArvoreDoJogo(new Mensagem(pergunta5), null, null);
-	      		inicio.setNodeDireita(node4);
-	      		node4.setNodeEsquerda(node5);
+	      		node4 = new ArvoreDoJogo(new Mensagem(pergunta5), null, null); 
+	      		inicio.setNodeDireita(node4); //iniciamos um novo caminho para a direita.
+	      		node4.setNodeEsquerda(node5); //na node4 colocamos um novo caminho para a esquerda
 	      		node5 = new ArvoreDoJogo(new Mensagem(pergunta6),new ArvoreDoJogo(new Mensagem(ANIMAL4),null,null), null);
 	      		node6 = new ArvoreDoJogo(new Mensagem(pergunta7),new ArvoreDoJogo(new Mensagem(ANIMAL5),null,null),null);
 	      		node5.setNodeDireita(node6);
 	      		node7 = new ArvoreDoJogo(new Mensagem(pergunta11),null,null);
-	      		node6.setNodeDireita(node7);
+	      		node6.setNodeDireita(node7);//caminho para a esquerda da node9
 	      		node8 = new ArvoreDoJogo(new Mensagem(pergunta12),new ArvoreDoJogo(new Mensagem(ANIMAL1),null,null),null);
 	      		node9= new ArvoreDoJogo(new Mensagem(pergunta13),new ArvoreDoJogo(new Mensagem(ANIMAL8),null,null),null);
-	      		node8.setNodeDireita(node9);
+	      		node8.setNodeDireita(node9); //caminho para a direita da node9
 	      		
 	      		node9 = new ArvoreDoJogo(new Mensagem(pergunta8),null,null);
-	      		node4.setNodeDireita(node9);
+	      		node4.setNodeDireita(node9); //vamos trabalhar agora no caminho para a direita da node4
 	      		node9 = new ArvoreDoJogo(new Mensagem(pergunta9),new ArvoreDoJogo(new Mensagem(ANIMAL2),null,null),null);
 	      		node10 = new ArvoreDoJogo(new Mensagem(pergunta10),new ArvoreDoJogo(new Mensagem(ANIMAL6),null,null),null);
-	      		node9.setNodeDireita(node10);
+	      		node9.setNodeDireita(node10); 
+	      		//TODO O PERCURSO PARA A DIREITA DO INICIO
+	      		
 		return inicio; 
 	    
 
