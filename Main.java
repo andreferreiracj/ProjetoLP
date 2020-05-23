7d565befa55df1c70d565916c3d18e3ffceba1fc
andreferreiracj <65228397+andreferreiracj@users.noreply.github.com>
2020-05-23 18:43:47 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -83,11 +83,12 @@ public class Main {
 	      		node9= new ArvoreDoJogo(new Mensagem(pergunta13),new ArvoreDoJogo(new Mensagem(ANIMAL8),null,null),null);
 	      		node8.setNodeDireita(node9); //caminho para a direita da node9
 	      		
-	      		node9 = new ArvoreDoJogo(new Mensagem(pergunta8),null,null);
-	      		node4.setNodeDireita(node9); //vamos trabalhar agora no caminho para a direita da node4
-	      		node9 = new ArvoreDoJogo(new Mensagem(pergunta9),new ArvoreDoJogo(new Mensagem(ANIMAL2),null,null),null);
-	      		node10 = new ArvoreDoJogo(new Mensagem(pergunta10),new ArvoreDoJogo(new Mensagem(ANIMAL6),null,null),null);
-	      		node9.setNodeDireita(node10); 
+	      		node10 = new ArvoreDoJogo(new Mensagem(pergunta8),null,null);
+	      		node4.setNodeDireita(node10); //vamos trabalhar agora no caminho para a direita da node4
+	      		node11 = new ArvoreDoJogo(new Mensagem(pergunta9),new ArvoreDoJogo(new Mensagem(ANIMAL2),null,null),null);
+	      		node10.setNodeEsquerda(node11);
+	      		node12 = new ArvoreDoJogo(new Mensagem(pergunta10),new ArvoreDoJogo(new Mensagem(ANIMAL6),null,null),null);
+	      		node9.setNodeDireita(node12); 
 	      		//TODO O PERCURSO PARA A DIREITA DO INICIO
 	      		
 		return inicio; 
