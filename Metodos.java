4a728141c70c9d7a9085dd2fb9864ff97e6a8499
andreferreiracj <65228397+andreferreiracj@users.noreply.github.com>
2020-06-14 11:41:10 +0100

    Add files via upload

---
+++ Metodos.java
@@ -0,0 +1,8 @@
+
+public interface Metodos {
+	public void consultar(int profundidade);
+	public void postordenar();
+	public void preorder();
+	public void ordenar();
+	public boolean estaVazio();
+}
